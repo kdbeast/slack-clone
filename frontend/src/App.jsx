4dd40dd73b34708be81d79e73e6c0f176e3afe81
Karan Jamwal <karanjamwal2004@gmail.com>
2025-10-22 21:21:28 +0530

    AuthProvider & tools setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,34 @@
-import {
-  SignedIn,
-  SignedOut,
-} from "@clerk/clerk-react";
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
 import HomePage from "./pages/HomePage";
 import AuthPage from "./pages/AuthPage";
 import { Routes, Route, Navigate } from "react-router";
 
+import * as Sentry from "@sentry/react";
+
+const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
+
 const App = () => {
   return (
     <>
+      <button
+        onClick={() => {
+          throw new Error("My test error from App.jsx");
+        }}
+      >
+        THROW ERROR
+      </button>
       <SignedIn>
-        <Routes>
+        <SentryRoutes>
           <Route path="/" element={<HomePage />} />
           <Route path="/auth" element={<Navigate to={"/"} replace />} />
-        </Routes>
+        </SentryRoutes>
       </SignedIn>
+
       <SignedOut>
-        <Routes>
+        <SentryRoutes>
           <Route path="/auth" element={<AuthPage />} />
           <Route path="/*" element={<Navigate to={"/auth"} replace />} />
-        </Routes>
+        </SentryRoutes>
       </SignedOut>
     </>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,22 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
-import { BrowserRouter } from "react-router";
+import {
+  BrowserRouter,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+import AuthProvider from "./providers/AuthProvider.jsx";
+
+import * as Sentry from "@sentry/react";
+
+const queryClient = new QueryClient();
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -11,11 +24,30 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
+Sentry.init({
+  dsn: "https://786552fb3febe5dc371c59960b193dab@o4510197746106368.ingest.us.sentry.io/4510233525747722",
+  integrations: [
+    Sentry.reactRouterV7BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+          <Toaster position="top-right" />
+        </QueryClientProvider>
       </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
